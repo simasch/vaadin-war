24fb7801a267b72a802aefe0bb523e245096a48a
Simon Martinelli <simon@martinelli.ch>
2025-04-29 19:45:23 +0200

    Added theme

--- src/main/java/ch/martinelli/demo/VaadinWarApplication.java
+++ src/main/java/ch/martinelli/demo/VaadinWarApplication.java
@@ -1,12 +1,15 @@
 package ch.martinelli.demo;
 
+import com.vaadin.flow.component.page.AppShellConfigurator;
+import com.vaadin.flow.theme.Theme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
+@Theme(value = "war")
 @SpringBootApplication
-public class VaadinWarApplication extends SpringBootServletInitializer {
+public class VaadinWarApplication extends SpringBootServletInitializer implements AppShellConfigurator {
 
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
--- src/main/java/ch/martinelli/demo/ui/MainView.java
+++ src/main/java/ch/martinelli/demo/ui/MainView.java
@@ -1,6 +1,6 @@
 package ch.martinelli.demo.ui;
 
-import com.vaadin.flow.component.html.H2;
+import com.vaadin.flow.component.html.H1;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Route;
 
@@ -8,7 +8,7 @@ import com.vaadin.flow.router.Route;
 public class MainView extends VerticalLayout {
 
     public MainView() {
-        H2 h2 = new H2("Hello Vaadin WAR!");
+        H1 h2 = new H1("Hello Vaadin WAR!");
         add(h2);
     }
 }
